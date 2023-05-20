56f860f29e81f41e7af3b531a8fb1a454c04d298
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-05-20 11:55:31 +0300

    Refactor

--- knowledge_graph/knowledge_graph_test.go
+++ knowledge_graph/knowledge_graph_test.go
@@ -5,51 +5,74 @@ import (
 )
 
 func Test_KnowledgeGraph_Encode(t *testing.T) {
-	// initialize empty graph
 	graph := &KnowledgeGraph{}
 
-	expected := "\n<empty knowledge graph>"
-	if graph.Encode() != expected {
-		t.Errorf("Encode() returned %q, expected %q", graph.Encode(), expected)
+	cases := []struct {
+		name  string
+		edges []*KGEdge
+		want  string
+	}{
+		{"Empty Graph", []*KGEdge{}, "\n<empty knowledge graph>"},
+		{"Non-Empty Graph 1", []*KGEdge{
+			{Head: "A", Relation: "REL", Tail: "B"},
+			{Head: "B", Relation: "REL", Tail: "C"},
+			{Head: "C", Relation: "REL", Tail: "D"},
+		}, "\n(A, REL, B)\n(B, REL, C)\n(C, REL, D)"},
 	}
 
-	// add a few edges
-	graph.Edges = []*KGEdge{
-		{Head: "A", Relation: "REL", Tail: "B"},
-		{Head: "B", Relation: "REL", Tail: "C"},
-		{Head: "C", Relation: "REL", Tail: "D"},
-	}
-
-	expected = "\n(A, REL, B)\n(B, REL, C)\n(C, REL, D)"
-	if graph.Encode() != expected {
-		t.Errorf("Encode() returned %q, expected %q", graph.Encode(), expected)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			graph.Edges = c.edges
+			if got := graph.Encode(); got != c.want {
+				t.Errorf("Encode() returned %v, want %v", got, c.want)
+			}
+		})
 	}
 }
 
 func Test_KnowledgeGraph_Schema(t *testing.T) {
 	graph := &KnowledgeGraph{}
-	expected := "<line-separated list of triplets of the form (head, relation, tail)>"
-	if graph.Schema() != expected {
-		t.Errorf("Schema() returned %q, expected %q", graph.Schema(), expected)
+
+	cases := []struct {
+		name string
+		want string
+	}{
+		{"Schema Test 1", "<line-separated list of triplets of the form (head, relation, tail)>"},
+		{"Schema Test 2", "<line-separated list of triplets of the form (head, relation, tail)>"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			if got := graph.Schema(); got != c.want {
+				t.Errorf("Schema() returned %v, want %v", got, c.want)
+			}
+		})
 	}
 }
 
 func Test_ParseKGEdge(t *testing.T) {
-	// test valid edge
-	s := "(A, REL, B)"
-	expected := &KGEdge{Head: "A", Relation: "REL", Tail: "B"}
-	edge, err := ParseKGEdge(s)
-	if err != nil {
-		t.Errorf("Parsing failed with error: %s", err)
-	}
-	if *edge != *expected {
-		t.Errorf("ParseKGEdge(%q) returned %q, expected %q", s, edge, expected)
+	cases := []struct {
+		name     string
+		input    string
+		wantEdge KGEdge
+		wantErr  bool
+	}{
+		{"Valid Edge", "(A, REL, B)", KGEdge{Head: "A", Relation: "REL", Tail: "B"}, false},
+		{"Invalid Edge", "(A, REL, )", KGEdge{}, true},
 	}
 
-	// test invalid edge
-	s = "(A, REL, )"
-	edge, err = ParseKGEdge(s)
-	if err == nil {
-		t.Errorf("Expected parsing to fail, but it succeeded")
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			edge, err := ParseKGEdge(c.input)
+			if err != nil {
+				if c.wantErr {
+					return
+				}
+				t.Fatalf("Unexpected error when parsing %q: %v", c.input, err)
+			}
+			if *edge != c.wantEdge {
+				t.Errorf("Parsed edge %v, want %v", *edge, c.wantEdge)
+			}
+		})
 	}
 }
