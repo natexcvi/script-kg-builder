3d3982f40b2aa875c8009141b3f232415d00292b
Yonatane12345 <yon12345@gmail.com>
2023-09-03 23:22:39 +0300

    code cleaned

--- face_detection/cluster_funcs.py
+++ face_detection/cluster_funcs.py
@@ -1,46 +1,24 @@
+""" Inspired from https://github.com/cppxaxa/FaceRecognitionPipeline_GeeksForGeeks"""
+
 import io
 import os
 import math
 import time
 from PIL import Image
 from sklearn.cluster import DBSCAN
-from imutils import build_montages, paths
 import numpy as np
-import os
-import pickle
 import cv2
 import shutil
-import time
 from tqdm import tqdm
 from moviepy.editor import *
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-
 from google.cloud import videointelligence_v1 as videointelligence
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\USER\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
 
 IMGS_PER_FACE = 5
-
-''' Common utilities '''
-'''
-Credits: AndyP at StackOverflow
-The ResizeUtils provides resizing function to keep the aspect ratio intact
-
-class ResizeUtils:
-    # Given a target height, adjust the image by calculating the width and resize
-    def rescale_by_height(self, image, target_height, method=cv2.INTER_LANCZOS4):
-        """Rescale `image` to `target_height` (preserving aspect ratio)."""
-        w = int(round(target_height * image.shape[1] / image.shape[0]))
-        return cv2.resize(image, (w, target_height), interpolation=method)
-
-    # Given a target width, adjust the image by calculating the height and resize
-    def rescale_by_width(self, image, target_width, method=cv2.INTER_LANCZOS4):
-        """Rescale `image` to `target_width` (preserving aspect ratio)."""
-        h = int(round(target_width * image.shape[0] / image.shape[1]))
-        return cv2.resize(image, (target_width, h), interpolation=method)
-'''
 
 def detect_faces(path):
     """Detects faces in a video."""
@@ -103,8 +81,7 @@ def extract_faces(annotation_result,movie_name,path,output):
         time.sleep(0.5)
     os.mkdir(all_dir)
     
-    # TODO movie = UPLOAD FROM GOOGLE CLOUD
-    #movie = VideoFileClip("D:\\OneDrive\\OneDrive - mail.tau.ac.il\\python\\face_detection\\12.Y34r5.4.5l4v3.13.br.sdm0v13sp01nt.c0m.mp4")
+    """ If movie is in Google Cloud, for this beta version need a local path for the extraction"""
     movie = VideoFileClip(path)
     i = 0
     
@@ -125,8 +102,6 @@ def extract_faces(annotation_result,movie_name,path,output):
                     i+=1
                     frame = os.path.join(output,"image"+str(i)+"full.jpg")
                     movie.save_frame(frame, t=(obj.time_offset.seconds + obj.time_offset.microseconds / 1e6), withmask=False)
-                    #if i < 5:
-                        #print(obj.time_offset.seconds + obj.time_offset.microseconds / 1e6)
                     box = obj.normalized_bounding_box
                     
                     # Opens a image in RGB mode
@@ -139,28 +114,14 @@ def extract_faces(annotation_result,movie_name,path,output):
                     
                     crop_im = im.crop((box.left*width, box.top*height, box.right*width, box.bottom*height))
                     
-                    #TODO keep the same ratios and sizes
-                    """ def AutoResize(self, frame):
-            resizeUtils = ResizeUtils()
-
-            height, width, _ = frame.shape
-
-            if height > 500:
-                frame = resizeUtils.rescale_by_height(frame, 500)
-                self.AutoResize(frame)
-            
-            if width > 700:
-                frame = resizeUtils.rescale_by_width(frame, 700)
-                self.AutoResize(frame)
-            
-            return frame """
-                    
                     crop_im.save(face_path)
                     os.remove(frame)
                     
     return os.path.join(output,"Faces")
 
 def mp_detect_and_extract(path,output):
+    
+    """ DETECT AND EXTRACT THE IMAGES USING MEDIAPIPE """
     
     if os.path.exists(output):
         shutil.rmtree(output)
@@ -215,16 +176,16 @@ def mp_detect_and_extract(path,output):
                 face_path = os.path.join(all_dir,str(frame_index)+"_"+str(face_index)+".jpg")
                 # Save the cropped image
                 cv2.imwrite(face_path, cropped_image)
-                
-                #print("frame: ", frame_index, " face: ", face_index, " saved, bbox: ", bbox, " cofidence: ", detection.categories[0].score)
         
     print(total_faces, " faces extracted")
     return os.path.join(output,"Faces")
             
-"""
-# Option - using mediapipe landmarks
+
+
 
 def landmark(faces_dir):
+    """ Option - using mediapipe landmarks """
+    
     #Create an FaceLandmarker object.
     base_options = python.BaseOptions(model_asset_path='face_landmarker.task')
     options = vision.FaceLandmarkerOptions(base_options=base_options,
@@ -244,7 +205,7 @@ def landmark(faces_dir):
         dict[id] = (img_path,[landmark for landmark in detection_result.face_landmarks])
         
     return dict
-"""
+
         
 def cluster(embeddings):
 
@@ -272,7 +233,7 @@ def cluster(embeddings):
                 flat_landmarks.append(0)
             landmarks_mat.append(flat_landmarks)
             print("couldn`t find face in image number ", id)
-    #print(landmarks_mat)
+            
     """
     
     # cluster the embeddings
@@ -304,33 +265,4 @@ def split_images(labels,faces_path):
         src = os.path.join(faces_path,"all",faces[i])
         dst = os.path.join(faces_path,"clustered",str(label),faces[i])
         shutil.copy(src, dst)
-        
-def cluster_naming(characters_embeddings, embeddings, output):
-    faces_path = os.path.join(output,"Faces")
-    characters_path = os.path.join(output,"characters")
-    clusters_path = os.path.join(faces_path,"clustered")
-    characters = os.listdir(characters_path)
-    for cluster in os.listdir(clusters_path):
-        if cluster != "-1":
-            cluster_path = os.path.join(clusters_path,cluster)
-            best_dist = np.inf
-            for character_i in range(len(characters)):
-                avg_dist = 0
-                character_embedding = characters_embeddings[character_i]
-                character_name = os.path.splitext(characters[character_i])[0]
-                if avg_dist == 0:
-                    print("embedding for character ", character_name, ":", character_embedding)
-                for img_i in tqdm(range(len(os.listdir(cluster_path))), desc = "calculating distance for cluster "+str(cluster)+" from character "+str(character_name)):
-                    if avg_dist == 0:
-                        print("embedding for: ", img_i, ":", embeddings[img_i])
-                    avg_dist += np.linalg.norm(embeddings[img_i]-character_embedding)
-                avg_dist = avg_dist/len(os.listdir(cluster_path))
-                print("avg dist: ", avg_dist)
-                if avg_dist < best_dist:
-                    best_dist = avg_dist
-                    best_character = character_name
-                    print("!!! NEW BEST CHARACTER")
-            os.rename(cluster_path,os.path.join(clusters_path,cluster+" "+best_character))
-            print("CHANGED CLUSTER ", cluster, " TO: ", best_character)
-            
                     
--- face_detection/cluster_pipeline.py
+++ face_detection/cluster_pipeline.py
@@ -1,8 +1,9 @@
-""" from https://github.com/cppxaxa/FaceRecognitionPipeline_GeeksForGeeks"""
+""" Inspired from https://github.com/cppxaxa/FaceRecognitionPipeline_GeeksForGeeks"""
 
 from cluster_funcs import *
-import argparse
 from face_embedding import *
+
+import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extracting faces from video and cluster it by person')
@@ -16,33 +17,35 @@ if __name__ == "__main__":
     if output == "output":
         output = os.path.join(os.getcwd(),movie)
         
-    #faces_path = mp_detect_and_extract(path,output) #USING MEDIAPIPE FRAME BY FRAME
+    #faces_path = mp_detect_and_extract(path,output) # USING MEDIAPIPE FRAME BY FRAME
     
-    ''' Retrieve faces boxes and times from a movie '''
+    ''' 1. Retrieve faces boxes and times from a movie '''
     annotation_result = detect_faces(path)
     print("retrieved faces")
     
-    ''' Extract faces pictures to a folder '''
+    ''' 2. Extract faces pictures to a folder '''
     faces_path = extract_faces(annotation_result,movie,path,output)
     print("extreacted faces images to: ", faces_path)
     
-    #faces_path = "D:\\OneDrive\\OneDrive - mail.tau.ac.il\\python\\script-kg-builder\\face_detection\\12.Y34r5.4.5l4v3\\Faces\\Faces"
+    """
+    Used to start from dir containing all the images. mark 1 + 2 as remarks
+    faces_path = "...\\movie_name\\faces"
+    """
     
-    ''' extrect embeddings for each face '''
-    #embeddings = pymain(os.path.join(faces_path,"all"))
-    #np.save(os.path.join(faces_path,"embeddings"),embeddings)
-    embeddings = np.load(os.path.join(faces_path,"embeddings.npy"))
+    ''' 3. extrect embeddings for each face '''
+    embeddings = pymain(os.path.join(faces_path,"all"))
+    np.save(os.path.join(faces_path,"embeddings"),embeddings)
     print("Extrected embeddings")
     
-    ''' Cluster the  faces by their landmarks '''
+    """
+    Used to start from file containing images embedding. mark 1 + 2 + 3 as remarks
+    embeddings = np.load(os.path.join(faces_path,"embeddings.npy"))
+    """
+    
+    ''' 4. Cluster the  faces by their landmarks '''
     labels = cluster(embeddings)
     print("images clustered: ", labels)
     
-    ''' Save the photos to different dirs '''
+    ''' 5. Save the photos to different dirs '''
     split_images(labels,faces_path)
     print("images splitted.")
-    
-    ''' Name the different clusters '''
-    #characters_embeddings = pymain(os.path.join(output,"characters"))
-    #print("got characters embeddings")
-    #cluster_naming(characters_embeddings, embeddings, output)
